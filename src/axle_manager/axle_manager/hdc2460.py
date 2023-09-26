26fe3ceaf1d461433cc322b1c6a3fa87688679d4
naterbots <20501643+naterbots@users.noreply.github.com>
2023-09-25 21:05:05 -0500

    Added basic config and commands. Lets hope nothing blows up

--- src/axle_manager/axle_manager/hdc2460.py
+++ src/axle_manager/axle_manager/hdc2460.py
@@ -1,36 +1,81 @@
 import serial
+from numpy import float32
 from utilities.utilities.tools import Tools
 
 class Hdc2460():
 
-    def __init__(self, serialPort:str,bitRate:int,maxSpeed:int):
+    def __init__(self, serialPort:str,bitRate:int,leftChannel:int,rightChannel:int):
         self.port = serial.Serial(serialPort, bitRate, timeout=2, xonxoff=False, rtscts=False, dsrdtr=False)
-        self.maxSpeed = maxSpeed
-
-    def  isConnected(self) -> bool:
-        return self.port.is_open
-
-    def sendCommand(self, cmd):
-        """Sends the serial data to both the HDC2460"""
-        self.port.write(cmd + "\r")
-
-    def move(self, left_speed:float, right_speed:float):
-        """Tells the HDC2460 to move at set speed"""
-        left_speed = round(left_speed*1000)
-        right_speed = round(right_speed*1000)
-
-        #clamp speed, format, and send it of to the roboteq
-        left_speed = Tools.clamp(left_speed, -self.maxSpeed, self.maxSpeed)
-        right_speed = Tools.clamp(right_speed, -self.maxSpeed, self.maxSpeed)
-
-        self.send_Command("!G 2 {left_speed}") 
-        self.send_Command("!G 1 {right_speed}")  
-
-    def resetDigitalOut(self, bit:int):
-        """"Will turn off digital output selected by the bit number."""
-        self.send_Command("!D0 {bit}")  
-
+        self.maxSpeed = 1000
+        self.leftChannel = leftChannel
+        self.rightChannel = rightChannel
 
     def close(self):
         self.port.close()
 
+    def  isConnected(self) -> bool:
+        return self.port.is_open
+
+    def move(self, left_speed:float32, right_speed:float32):
+        """Tells both motors to move at set speed. Assumes speed input from -1 to 1."""
+        self.setMotor(self.leftChannel,left_speed)
+        self.setMotor(self.rightChannel,right_speed)
+
+    def configure(self, maxSpeed:int,accelRate:int, brakeRate:int):
+        self.setAccelration(accelRate)
+        self.setDeccelration(brakeRate)
+        self.maxSpeed = maxSpeed
+
+
+#region Roboteq Driver
+    def sendCommand(self, cmd):
+        """Sends the serial data to both the HDC2460"""
+        self.port.write(cmd + "\r")
+
+    def setMotor(self,ch:int,speed:float32):
+        """Tells the HDC2460 to move at set speed"""
+        speed = round(speed*1000)
+
+        #clamp speed, format, and send it of to the roboteq
+        speed = Tools.clamp(speed, -self.maxSpeed, self.maxSpeed)
+
+        #Roboteq speed input is from -1000 to 1000. Just in case max speed > 1000.
+        speed = Tools.clamp(speed, -1000, 1000)
+        #send command to 
+        self.sendCommand("!G {ch} {speed}")
+
+    def stopMotor(self, ch:int):
+        """Stops the motor for the specified channel."""
+        self.sendCommand("!MS {ch}")
+
+    def stopAllMotors(self):
+        """Stops both motors on the controller."""
+        self.stopMotor(self.leftChannel)
+        self.stopMotor(self.rightChannel)
+
+    def resetDigitalOut(self):
+        """"Will turn off all digital outputs."""
+        self.sendCommand("!DS 0")  
+
+    def setDigitalOut(self,bit:int,val:int):
+        """"Will set digital output selected by the bit number."""
+        if val==0:
+            self.sendCommand("!D0 {bit}")  
+        else:
+            self.sendCommand("!D1 {bit}")  
+
+    def setDeccelration(self, rate:int):
+        """Set the rate of speed change during decceleration for a motor channel."""
+        rate = Tools.clamp(rate, 0, 500000)
+        self.sendCommand("!DC {self.leftChannel} {rate}")  
+        self.sendCommand("!DC {self.rightChannel} {rate}")  
+
+    def setAccelration(self, rate:int):
+        """Set the rate of speed change during acceleration for a motor channel."""
+        rate = Tools.clamp(rate, 0, 500000)
+        self.sendCommand("!AC {self.leftChannel} {rate}")  
+        self.sendCommand("!AC {self.rightChannel} {rate}")  
+
+
+
+#endregion
--- src/axle_manager/axle_manager/hdc2460_node.py
+++ src/axle_manager/axle_manager/hdc2460_node.py
@@ -6,24 +6,40 @@ from axle_manager.hdc2460 import Hdc2460
 class Hdc2460Node(Node):
     def __init__(self):
         super().__init__("hdc2460")
-        self.declare_parameter('facSerialPort',rclpy.Parameter.Type.STRING)
-        self.declare_parameter('bacSerialPort',rclpy.Parameter.Type.STRING)
-        self.declare_parameter('serialBitRate',rclpy.Parameter.Type.INTEGER)
-        self.declare_parameter('maxSpeed',rclpy.Parameter.Type.INTEGER)
+        self.declare_parameters(
+            parameters=[
+                ('facSerialPort',rclpy.Parameter.Type.STRING),
+                ('bacSerialPort',rclpy.Parameter.Type.STRING),
+                ('serialBitRate',rclpy.Parameter.Type.INTEGER),
+                ('leftChannel',1,rclpy.Parameter.Type.INTEGER),
+                ('rightChannel',2,rclpy.Parameter.Type.INTEGER),
+                ('maxSpeed',1000,rclpy.Parameter.Type.INTEGER),
+                ('accelRate',500,rclpy.Parameter.Type.INTEGER),
+                ('brakeRate',500,rclpy.Parameter.Type.INTEGER)
+            ]
+        )
 
         self.subscription = self.create_subscription(Twist, '/cmd_vel', self.callback, 10)
         self.subscription  # prevent unused variable warning
         
-
         facSerialPort = self.get_parameter('facSerialPort')
         bacSerialPort = self.get_parameter('bacSerialPort')
         bitRate = self.get_parameter('serialBitRate')
+        leftChannel = self.get_parameter('leftChannel')
+        rightChannel = self.get_parameter('rightChannel')
         maxSpeed = self.get_parameter('maxSpeed')
+        accelRate = self.get_parameter('accelRate')
+        brakeRate = self.get_parameter('brakeRate')
 
-        self.fac = Hdc2460(facSerialPort,bitRate,maxSpeed)
-        self.bac = Hdc2460(bacSerialPort,bitRate,maxSpeed)
+        self.fac = Hdc2460(facSerialPort,bitRate,maxSpeed,leftChannel,rightChannel)
+        self.bac = Hdc2460(bacSerialPort,bitRate,maxSpeed,leftChannel,rightChannel)
 
         if self.fac.isConnected and self.bac.isConnected:
+            self.get_logger().info("HDC2460s connected.")
+            self.fac.configure(maxSpeed,accelRate,brakeRate)
+            self.get_logger().debug("FAC configured.")
+            self.bac.configure(maxSpeed,accelRate,brakeRate)
+            self.get_logger().debug("BAC configured.")
             self.get_logger().info("HDC2460s initialized.")
         else:
             self.get_logger().error("Unable to initialize Hdc2460s")
