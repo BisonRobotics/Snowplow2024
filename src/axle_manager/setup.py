00d8ad744b98a0dfbe14a03179e881e3accef5e7
naterbots <20501643+naterbots@users.noreply.github.com>
2023-10-17 21:14:49 -0500

    Ready to test on robot

--- src/axle_manager/setup.py
+++ src/axle_manager/setup.py
@@ -1,4 +1,5 @@
 from setuptools import find_packages, setup
+from glob import glob
 
 package_name = 'axle_manager'
 
@@ -10,6 +11,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/'+package_name,glob('config/*params.[yaml]*'))
     ],
     install_requires=['setuptools','serial'],
     zip_safe=True,
