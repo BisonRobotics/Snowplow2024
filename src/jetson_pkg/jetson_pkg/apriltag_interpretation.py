a111cbb618eb4cd2cc21b78942cbfd2173024549
brandongasser <bgasser12@gmail.com>
2024-01-21 21:37:09 -0600

    allow for multiple tags (untested)

--- src/jetson_pkg/jetson_pkg/apriltag_interpretation.py
+++ src/jetson_pkg/jetson_pkg/apriltag_interpretation.py
@@ -1,6 +1,6 @@
 import math
 
-def april_tag_interpretation(xa,ya,thetaa,xc,zc,thetac):
+def apriltag_interpretation(xa,ya,thetaa,xc,zc,thetac):
     thetar=math.radians(thetaa) +math.radians(thetac) + math.pi + math.atan(xc/zc)
    
     dist = math.sqrt(xc**2 +zc**2)
@@ -9,7 +9,7 @@ def april_tag_interpretation(xa,ya,thetaa,xc,zc,thetac):
     return (xr,yr,(thetar*(180/math.pi)) %  (2*math.pi))
 
 if __name__ == "__main__":
-    xr,yr,thetar = april_tag_interpretation(10,0,270,1,1.732,30)
+    xr,yr,thetar = apriltag_interpretation(10,0,270,1,1.732,30)
 
     # Verbose
     print(f"xr: {xr}")
--- src/jetson_pkg/jetson_pkg/jetson_apriltag.py
+++ src/jetson_pkg/jetson_pkg/jetson_apriltag.py
@@ -1,17 +1,22 @@
 import rclpy
 from rclpy.node import Node
 
-from geometry_msgs.msg import Point
+from geometry_msgs.msg import Twist
+from jetson_pkg.apriltag_interpretation import apriltag_interpretation
 
 import math
 import numpy as np
 import threading
 import cv2
 import apriltag
+import json
 
 cap = cv2.VideoCapture('rtsp://admin:hyflex@192.168.1.131:80/cam/realmonitor?channel=1&subtype=0')
 detector = apriltag.Detector()
 fx, fy, cx, cy = (1071.1362274102335, 1102.1406887400624, 953.030188084331, 468.0382502048589)
+
+pivot_x_offset = -0.017
+pivot_z_offset = 0.83
 
 class ApriltagPublisher(Node):
     def __init__(self):
@@ -21,6 +26,8 @@ class ApriltagPublisher(Node):
         self.latest_frame = None
         threading.Thread(target=self.keep_up_thread).start()
         self.timer = self.create_timer(timer_period, self.timer_callback)
+        with open('apriltag_poses.json') as json_file:
+            self.apriltag_poses = json.load(json_file)
 
     def keep_up_thread(self):
         while True:
@@ -32,12 +39,30 @@ class ApriltagPublisher(Node):
         gray = cv2.cvtColor(self.latest_frame, cv2.COLOR_RGB2GRAY)
         detections = detector.detect(gray)
         if len(detections) > 0:
-            pose, _, _ = detector.detection_pose(detections[0], [fx,fy,cx,cy], 0.3254375)
-            self.get_logger().info(f'x={pose[0][3]} meters; y={pose[1][3]} meters; z={pose[2][3]} meters; rotation={np.arcsin(-pose[2][0]) * (180 / math.pi)} degrees')
-            msg = Point()
-            msg.x = 0.0
-            msg.y = 0.0
-            msg.z = 0.0
+            msg = Twist()
+            msg.linear.x = 0
+            msg.linear.z = 0
+            msg.angular.y = 0
+            
+            for detection in detections:
+                pose, _, _ = detector.detection_pose(detection, [fx, fy, cx, cy], 0.3254375)
+                
+                relative_x = pose[0][3] + pivot_x_offset
+                relative_y = pose[2][3] + pivot_z_offset
+                relative_rotation = np.arcsin(-pose[2][0]) * (180 / math.pi)
+                
+                apriltag_pose = self.apriltag_poses[detection.family][detection.id]
+                
+                xr, zr, thetar = apriltag_interpretation(apriltag_pose['x'], apriltag_pose['z'], apriltag_pose['angle'], relative_x, relative_y, relative_rotation)
+                
+                msg.linear.x += xr
+                msg.linear.z += zr
+                msg.angular.y += thetar
+                
+            msg.linear.x /= len(detections)
+            msg.linear.z /= len(detections)
+            msg.angular.y /= len(detections)
+            
             self.publisher_.publish(msg)
         
 def main(args=None):
